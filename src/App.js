3d44af9a41bed8ee693a424dcf335fcc16f67567
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-12 15:16:33 +0600

    added some news

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Route, Switch } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Contacts from './components/Contacts/Contacts';
@@ -6,18 +7,24 @@ import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
 function App() {
+
+  const activeLinkStyle = {
+    fontSize: "18px"
+  }
+
   return (
     <div className="App">
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contacts">Contacts</a></li>
+        <li><NavLink exact activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/">Home</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/About">About</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/contacts">Contacts</NavLink></li>
       </ul>
       <Switch>
         <Route path="/about" component={About} />
         <Route path="/contacts" component={Contacts} />
         <Route exact path="/" component={Home} />
         <Route path="/" component={NotFound} />
+        {/* <Redirect to="/" /> */}
       </Switch>
     </div>
   );
