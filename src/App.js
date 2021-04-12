e11916e0933e5f1a59d8626f7f11f86749ec2e51
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-12 15:19:45 +0600

    added Posts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About/About';
 import Contacts from './components/Contacts/Contacts';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
+import Posts from './components/Posts/Posts';
 
 function App() {
 
@@ -17,10 +18,12 @@ function App() {
       <ul>
         <li><NavLink exact activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/">Home</NavLink></li>
         <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/About">About</NavLink></li>
+        <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/posts">Posts</NavLink></li>
         <li><NavLink activeClassName="activeLink" activeStyle = {activeLinkStyle} to="/contacts">Contacts</NavLink></li>
       </ul>
       <Switch>
         <Route path="/about" component={About} />
+        <Route path="/posts" component={Posts} />
         <Route path="/contacts" component={Contacts} />
         <Route exact path="/" component={Home} />
         <Route path="/" component={NotFound} />
