e7d31a36313c5cb309b800414a228d493b346222
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-12 14:23:12 +0600

    corected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import { Route } from 'react-router';
 import './App.css';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      This is App.js
+      <Route path="/about" component={About} />
+      <About />
     </div>
   );
 }
