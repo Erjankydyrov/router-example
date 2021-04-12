4d6fac5f21d44fe0b9c58eb1f0d5362dac8273b3
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-12 15:28:36 +0600

    added PostList

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,8 +1,12 @@
+import PostList from "./PostList/PostList";
 import classes from "./Posts.module.css";
 
 const Posts = () => {
     return ( 
-        <div className={classes.Posts}></div>
+        <div className={classes.Posts}>
+            Here posts
+            <PostList />
+        </div>
     );
 }
  
