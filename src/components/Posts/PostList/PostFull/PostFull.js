86e998f00da2451305ef2d8f865101cbd4e77547
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-12 15:33:46 +0600

    added new component

---
+++ src/components/Posts/PostList/PostFull/PostFull.js
@@ -0,0 +1,11 @@
+import classes from "./PostFull.module.css";
+
+const Posts = () => {
+    return ( 
+        <div className={classes.PostFull}>
+            Post
+        </div>
+    );
+}
+ 
+export default Posts;
