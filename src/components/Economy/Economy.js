362d0eea85d716ffaa8030b4ff5445b24f6ab301
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 14:33:46 +0600

    added new component

---
+++ src/components/Economy/Economy.js
@@ -0,0 +1,21 @@
+import classes from "./Economy.module.css";
+import img from "../../images/envelope.svg";
+
+const Economy = () => {
+  return (
+    <div className={classes.Economy}>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Economy;
