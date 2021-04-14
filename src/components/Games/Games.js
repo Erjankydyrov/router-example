5a562c1ec33b59fdb8c24b4639c269ce7d4dd0a4
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 12:52:01 +0600

    added new component

---
+++ src/components/Games/Games.js
@@ -0,0 +1,11 @@
+import classes from "./Games.module.css";
+
+const Games = () => {
+    return ( 
+        <div className={classes.Games}>
+            
+        </div>
+    );
+}
+ 
+export default Games;
