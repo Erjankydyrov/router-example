d90a23936374147cacc8035c40a918f3a0252e4e
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 12:54:20 +0600

    corected name

---
+++ src/components/WorldNews/WorldNews.js
@@ -0,0 +1,11 @@
+import classes from "./WorldNews.module.css";
+
+const WorldNews = () => {
+    return ( 
+        <div className={classes.WorldNews}>
+            
+        </div>
+    );
+}
+ 
+export default WorldNews;
