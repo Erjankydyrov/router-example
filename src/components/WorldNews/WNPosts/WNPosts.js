78a040ce5f4014ab01e2a8e382bd5acd91d0013e
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 12:59:06 +0600

    added new component

---
+++ src/components/WorldNews/WNPosts/WNPosts.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import classes from "./WNPosts.module.css";
+
+const WNPosts = () => {
+
+    const results = [];
+    for (let id = 1; id <=5; id++) {
+        results.push(<li><Link to={"/WorldNews/" + id}>Post number #{id}</Link></li>)
+    }
+    return ( 
+        <ul className={classes.WNPosts}>{results}</ul>
+    );
+}
+ 
+export default WNPosts;
--- src/components/WorldNews/WorldNews.js
+++ src/components/WorldNews/WorldNews.js
@@ -3,7 +3,9 @@ import classes from "./WorldNews.module.css";
 const WorldNews = () => {
     return ( 
         <div className={classes.WorldNews}>
-            
+            { match.isExact 
+            ? <PostList /> 
+            : <Route path="/posts/:id" component={PostFull} />}
         </div>
     );
 }
