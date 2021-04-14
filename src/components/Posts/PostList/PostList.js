6c5a7001440e1a8deb4b47d9ccc15374a77b22f4
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 15:31:31 +0600

    corected colors

--- src/components/Posts/PostList/PostList.js
+++ src/components/Posts/PostList/PostList.js
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import classes from "./PostList.module.css";
+import city from "../../../images/envelope.svg";
 
 const PostList = () => {
 
     const results = [];
     for (let id = 1; id <=5; id++) {
-        results.push(<li><Link to={"/posts/" + id}>Post number #{id}</Link></li>)
+        results.push(<li>
+                        <img src={city} alt=""/>
+                        <Link to={"/posts/" + id}>Post number #{id}</Link>
+                    </li>)
     }
     return ( 
         <ul className={classes.PostList}>{results}</ul>
