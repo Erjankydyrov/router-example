19f899ce9e51d900c8ce28322d63bd5ee13a60a9
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 14:43:21 +0600

    corected app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Redirect, Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
