fca2853410d1113996ff7e4083bba10f1d95d389
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-14 14:52:41 +0600

    added some news

--- src/components/Economy/Economy.js
+++ src/components/Economy/Economy.js
@@ -14,6 +14,76 @@ const Economy = () => {
           montes, nascetur ridiculus mus.
         </p>
       </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
+      <div className={classes.titleCom}>
+        <img src={img} alt="" />
+        <h4>Title</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a enim
+          dignissim, egestas justo id, aliquam augue. Phasellus maximus eu odio
+          sed posuere. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus.
+        </p>
+      </div>
     </div>
   );
 };
