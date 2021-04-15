4968035133dfc95546ff2a8569f54e62f2a33e9f
Erjankydyrov <erjankydyrov29@gmail.com>
2021-04-15 22:48:02 +0600

    added some news in WorldVolut

--- src/components/WorldVolut/WorldVolut.js
+++ src/components/WorldVolut/WorldVolut.js
@@ -1,9 +1,13 @@
 import classes from "./WorldVolut.module.css";
+import diagram from "../../images/diagram.png"
 
 const WorldVolut = () => {
     return ( 
         <div className={classes.WorldVolut}>
-
+            <div className={classes.voluta}>
+                <h2>Dollar</h2>
+                <img src={diagram} className={classes.diagram} />
+            </div>
         </div>
     );
 }
